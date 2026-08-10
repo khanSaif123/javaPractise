64363bb6efa2b94d1a7f23b9fef59a180fa967ba
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-10 14:29:47 +0530

    hollow square probleum solved

--- pattern.js
+++ pattern.js
@@ -1,46 +1,63 @@
-// const readline = require("readline")
+// // const readline = require("readline")
 
-// const rl = readline.createInterface({
+// // const rl = readline.createInterface({
+// //   input: process.stdin,
+// //   output: process.stdout
+// // })
+
+// // rl.question("Enter your age: ", (input) => {
+// //   let age = Number(input);
+
+// // })
+
+// let month = [
+//   "January",
+//   "Feb",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+
+// const readline = require("readline");
+
+// let rl = readline.createInterface({
 //   input: process.stdin,
-//   output: process.stdout
-// })
+//   output: process.stdout,
+// });
 
-// rl.question("Enter your age: ", (input) => {
-//   let age = Number(input);
+// rl.question("Enter Month Number: ", (monthNumber) => {
+//   console.log("Month number: ", monthNumber);
 
-// })
-
-let month = [
-  "January",
-  "Feb",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-const readline = require("readline");
-
-let rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Enter Month Number: ", (monthNumber) => {
-  console.log("Month number: ", monthNumber);
-
-  if(monthNumber >= 1 && monthNumber <= 12){
-    let monthIs = month[monthNumber - 1];
-    console.log(monthIs);
-  }else {
-    console.log('Invalid month number');
-  }
-});
+//   if(monthNumber >= 1 && monthNumber <= 12){
+//     let monthIs = month[monthNumber - 1];
+//     console.log(monthIs);
+//   }else {
+//     console.log('Invalid month number');
+//   }
+// });
 
 
+let printStar = 5;
+let lines = 4;
+let lineCount = 0;
+let star = "";
+
+while(lineCount < lines){
+    for(let i = 1; i<=printStar; i++){
+       
+      if(star.length < printStar){
+         star = star + "*";
+      }
+        
+    }
+    console.log(star)
+    lineCount++;
+    
+}
