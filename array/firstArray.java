99f553af8e0cde1e5b33620fbbc56bf5b81972e3
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-20 14:40:21 +0530

    learned how to traverse on array and print the values on each index

--- array/firstArray.java
+++ array/firstArray.java
@@ -1,9 +1,26 @@
 package array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * firstArray
  */
 public class firstArray {
 
-    
+    public static void main(String args[]){
+        // how to declare array.
+        // type[] arrayName = new type[size].
+        int [] masks = new int[3];
+        masks[0] = 20;
+        masks[1] = 50;
+        masks[2] = 45;
+
+        // System.out.println(masks[0]);
+        // System.out.println(masks);
+        // System.out.println(Arrays.toString(masks));
+        for(int i=0; i< masks.length; i++){
+            System.out.print(masks[i]+" ");
+        }
+    }
 }
