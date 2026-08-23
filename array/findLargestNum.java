6a1bb55f1ccc2e52af89d274c2dec100913cd56c
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-23 11:08:34 +0530

    some changes in the same question

--- array/findLargestNum.java
+++ array/findLargestNum.java
@@ -1,18 +1,23 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class findLargestNum {
     public static void main(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array");
+        System.out.println("Enter the size of the array:");
         int size = sc.nextInt();
 
         int arr [] = new int[size];
         int largest = 0;
+
+        System.out.println("Enter the values of array:");
         for(int i=0; i<size; i++){
             arr[i] = sc.nextInt();
         }
+
+        System.out.println("Your Enter this array: " + Arrays.toString(arr));
 
         for(int i=0; i<size; i++){
             if(arr[i] > largest){
