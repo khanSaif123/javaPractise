357012f99cb7f57b2b3a765cb3199e8209c2e3c4
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-24 15:51:28 +0530

    pairSum question solved

--- array/pairSum.java
+++ array/pairSum.java
@@ -1,5 +1,17 @@
 package array;
 
 public class pairSum {
-    
+    public static void main() {
+        int arr[] = { 2, 7, 11, 9 };
+        int target = 11;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                sum = arr[i] + arr[j];
+                if (sum == target) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + sum);
+                }
+            }
+        }
+    }
 }
