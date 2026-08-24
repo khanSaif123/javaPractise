a7415f1a6a474c8dac4e4bb0aa69d62610a546f1
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-24 14:47:20 +0530

    question solved odd even count

--- array/allOccurance.java
+++ array/allOccurance.java
@@ -2,6 +2,13 @@ package array;
 
 public class allOccurance {
     public static void main(){
-        
+        int arr [] = {10, 20, 30, 10};
+        int search = 10;
+
+        for(int i=0; i<arr.length; i++){
+            if(search == arr[i]){
+                System.out.println(search + " Found at index " + i);
+            }
+        }
     }
 }
