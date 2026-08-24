2fb25fd6160dfc430efca849c35363e45793606c
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-24 13:15:35 +0530

    one more basic queation on array solved find average

---
+++ array/average.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class average {
+    public static void main(){
+       Scanner sc = new Scanner(System.in);
+
+       int count = 0;
+       int average = 0;
+
+       System.out.println("Enter the size of the array : ");
+       int size = sc.nextInt();
+
+       int arr [] = new int[size];
+       System.out.println("Now enter the values in array: ");
+       for(int i=0; i<size; i++){
+        arr[i] = sc.nextInt();
+       }
+
+       System.out.println("Calculating the average.... ");
+       for(int i=0; i<size; i++){
+          count = count + arr[i];
+       }
+
+       average = count/size;
+       System.out.println("The average is: " + average);
+
+    }
+}
---
+++ array/calculateSum.java
@@ -0,0 +1,13 @@
+package array;
+
+public class calculateSum {
+    public static void main(){
+        int sum = 0;
+        int arr [] = {1, 23, 46, 10};
+
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("The sum = " + sum);
+    }
+}
