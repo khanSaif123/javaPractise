4957da4ecbed34952ae91d321b89059d6d85a03b
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-26 18:18:17 +0530

    lenear search

--- array/level2/lenearSearch.java
+++ array/level2/lenearSearch.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class lenearSearch {
     public static void main() {
+        // lenear search.
         Scanner sc = new Scanner(System.in);
         boolean found = false;
 
