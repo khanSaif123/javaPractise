57891358d12e3e2a20a4cc90f5e75a015f19bd14
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-30 21:42:56 +0530

    failed to solve remove duplicate

--- array/level3/removeDuplicate.java
+++ array/level3/removeDuplicate.java
@@ -1,7 +1,25 @@
 package array.level3;
 
+import java.util.Arrays;
+
 public class removeDuplicate {
     public static void main(String[] args) {
+        int arr[] = {10, 20, 10, 30, 40};
+        int newArray [] = new int[arr.length];
+
+        int i = 0;
+        int p = arr[arr.length - i - 1];
+        while(i < arr.length - 1){
+            if(arr[i] != arr[p]){
+                newArray[i] = arr[p];
+                i++;
+                p = arr[arr.length - i - 1];
+            }else{
+                i++;
+                p = arr[arr.length - i - 1];
+            }
+        }
         
+        System.out.println("new array without duplicate element is -> " + Arrays.toString(newArray));
     }
 }
