58c9dbfafad151854845d2cf4ade82726021af94
khanSaif@123 <khansaiflko0@gmail.com>
2026-09-03 12:40:41 +0530

    2d array basic start

---
+++ array2D/basic.java
@@ -0,0 +1,13 @@
+package array2D;
+
+public class basic {
+  public static void main(String args[]) {
+    int arr [][] = {{1,2,3}, {4,5,6}};
+     
+    for(int i=0; i<=arr.length; i++){
+      for(int j=0; j<=arr.length; j++){
+        System.out.println(arr[i][j]);
+      }
+    }
+  }
+}
