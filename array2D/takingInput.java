dca36b642b93089a0dbe4bd1384759eacb7dcd5e
khanSaif@123 <khansaiflko0@gmail.com>
2026-09-04 11:36:37 +0530

    learn how to take input of 2d array

---
+++ array2D/takingInput.java
@@ -0,0 +1,36 @@
+package array2D;
+
+import java.util.Scanner;
+
+public class takingInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of rows");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter no. of cols");
+
+        int cols = sc.nextInt();
+
+        int arr[][] = new int[rows][cols];
+
+        System.out.println("Enter the values of arrays.");
+        // outer loop handle rows.
+        for (int i = 0; i < rows; i++) {
+            // inner loop handle cols.
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Printing....");
+        // printing.
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
